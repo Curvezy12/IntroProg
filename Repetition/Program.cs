e547281d298e89f364620f35cbc074d28a019fe0
Curvezy <Curvezy12@gmail.com>
2021-03-23 14:15:24 +1100

    update

--- Repetition/Program.cs
+++ Repetition/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace Repetition
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Repetition {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("How Many Times Should I Print It? ");
+            string number = Console.ReadLine();
+
+            int counter = 0;
+
+            while(counter < int.Parse(number)) {
+                Console.WriteLine(name);
+                Console.Write("Enter Name: ");
+
+                name = Console.ReadLine();
+
+                counter++;
+            }
         }
     }
 }
