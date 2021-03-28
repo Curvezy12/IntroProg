1b39181d9e842c43a1bfd70f5879b0a1db73fbd4
Seb Conway <103641460@student.swin.edu.au>
2021-03-28 17:04:25 +1100

    Adding dekstop files

---
+++ Week1/VariablesTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VariablesTask {
+    class Program {
+        static void Main(string[] args) {
+                // Gathering Information
+                String Firstname;
+                String Surname;
+                String StudentID;
+                String YOB;
+                String CurrentYear;
+
+                Console.WriteLine("First Name: "); {
+                Firstname = Console.ReadLine();
+                }
+                Console.WriteLine("Surname: "); {
+                Surname = Console.ReadLine();
+                }
+                Console.WriteLine("StudentID: "); {
+                StudentID = Console.ReadLine();
+                } 
+                Console.WriteLine("Year Of Birth: "); {
+                YOB = Console.ReadLine();
+                } 
+                Console.WriteLine("Current year: "); {
+                CurrentYear = Console.ReadLine();
+                }
+                //converting into integers
+                int YOBint;
+                int CurrentYearint;
+
+                YOBint = int.Parse(YOB);
+                CurrentYearint = int.Parse(CurrentYear);
+
+                // Displaying Information
+               Console.WriteLine("Your Name: " + (Firstname + " " + Surname)); {
+               }
+               Console.WriteLine("Your Age: " + (CurrentYearint - YOBint)); {
+               }
+        }
+    }
+}
---
+++ Week2/SelectionTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfStatements {
+    class Program {
+        static void Main(string[] args) {
+
+            String age;
+
+            //ask for age 
+            Console.WriteLine("Age: "); 
+            age = Console.ReadLine();
+
+            //turn string into an integer and check to see if it is a valid input
+
+            if(int.TryParse(age, out int number)) {
+                if(number > 17) {
+                    Console.WriteLine("Legally adult age.");
+                } else {
+                    Console.WriteLine("Legally not adult age.");
+                }
+            } else {
+                Console.WriteLine("Invalid Input.");
+            }
+        }
+    }
+}
