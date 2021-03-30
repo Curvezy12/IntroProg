9cff8ad0db4fbf9f748126b2388321b114c30aeb
Curvezy <103641460@student.swin.edu.au>
2021-03-30 14:46:34 +1100

    NestedRepetitionComplete

--- NestedRepetition/Program.cs
+++ NestedRepetition/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace NestedRepetition
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace NestedRepetition {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+
+            int counter = 0;
+            int counter2;
+
+            while (counter < 10) {
+
+                counter = counter + 1;
+                counter2 = counter;
+
+                while (counter2 < 5) {
+
+                    Console.WriteLine(name);
+                    counter2 = counter2 + 1;
+                }   
+                Console.WriteLine();
+            }
+            Console.WriteLine("Goodbye " + name);
+        }   
     }
 }
