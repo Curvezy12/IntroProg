04350a203ab2ff735122976a53ff3abf4b296ff2
Curvezy <103641460@student.swin.edu.au>
2021-05-04 12:46:15 +1000

    created assessment task 1

---
+++ AssessmentTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AssessmentTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
