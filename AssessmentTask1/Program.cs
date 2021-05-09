1f532f39f85ec5325a9dcf592e062323446bdef6
Curvezy <103641460@student.swin.edu.au>
2021-05-10 08:39:24 +1000

    update

--- AssessmentTask1/Program.cs
+++ AssessmentTask1/Program.cs
@@ -21,22 +21,22 @@ namespace AssessmentTask1
                 int sum = 0;
                 int Num = NumOfRolls;
                 int[] DiceRoll;
-                float average = 0.0F;
+                float Average = 0.0F;
                 DiceRoll = new int[Num];
                     for(int i = 0; i <= Num - 1; i++) {
                         DiceRoll[i] = random.Next(7);
-                        sum +=  DiceRoll[i];
+                        Sum +=  DiceRoll[i];
                     }
-                    average = (float)sum / DiceRoll.Length;
+                    Average = (float)Sum / Num;
                     if (Start == "yes") {
                         Console.WriteLine("The Dice rolled a " + string.Join(", ", DiceRoll));
-                        Console.WriteLine("The Average was " + average);
-                        Console.WriteLine("The total was " + sum);
+                        Console.WriteLine("The Average was " + Average);
+                        Console.WriteLine("The total was " + Sum);
                     }
                 Console.WriteLine("Would you like to role again?");
                 Start = Console.ReadLine();
             }
             Console.WriteLine("Okay Goodbye.");
         }
-    }
+    } 
 }
