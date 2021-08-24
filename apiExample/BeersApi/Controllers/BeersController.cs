098130aa3199da9f7bbf6bfb17bcd5be02eef147
Curvezy12 <103641460@student.swin.edu.au>
2021-08-24 10:21:17 +1000

    beersapi

--- apiExample/BeersApi/Controllers/BeersController.cs
+++ apiExample/BeersApi/Controllers/BeersController.cs
@@ -1,22 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
-using BeersLib;
 using System.Collections.Generic;
+using BeersLib;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BeersApi.Controllers {
     [ApiController]
-    [Route("[controller]")]
-    public class BeersController: ControllerBase {
+    [Route ("[controller]")]
+    public class BeersController : ControllerBase {
 
         Beers beers = new Beers();
 
         [HttpGet]
-        public List<Beer> Get() {
+        public List<Beer> Get () {
             return beers.BeersList;
         }
-        
+
         [HttpGet("{name}")]
         public Beer GetBeer(string name) {
-            return beers.GetBeerByName;
+            return beers.GetBeerByName(name);
         }
     }
+
 }
--- apiExample/BeersLib/Beers.cs
+++ apiExample/BeersLib/Beers.cs
@@ -1,43 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace BeersLib
-{
+namespace BeersLib {
     public class Beer {
 
         public string Name { get; set; }
-
         public string Brewery { get; set; }
-
-        // Abv: Alchohol % by volume
+        // Abv: Alcohol By Volume
         public float Abv { get; set; }
-
-        // Ibu: International bitterness unit
-        public int Ibu { get; set; }
-
+        // IBU: Internation Bitterness Unit
+        public uint Ibu { get; set; }
         public int Amount { get; set; }
-
         public float Cost { get; set; }
-
-        public bool Open { get; set;}
+        public bool Open { get; set; }
 
         public Beer () {}
-        public Beer (string name, string brewery, float abv, int ibu, int amount, float cost) 
-        {
+        public Beer (string name, string brewery, float abv, uint ibu, int amount, float cost) {
             this.Name = name;
             this.Brewery = brewery;
             this.Abv = abv;
-            this.Ibu = ibu; 
+            this.Ibu = ibu;
             this.Amount = amount;
             this.Cost = cost;
-            this.Open = false;    
+            this.Open = false;
         }
 
-        public int Drink(int amount) {
-            throw new NotImplementedException();
+        public int Drink (int amount) {
+            throw new NotImplementedException ();
         }
-        public bool OpenBeer() {
-            throw new NotImplementedException();
+
+        public bool OpenBeer () {
+            throw new NotImplementedException ();
         }
     }
 
@@ -45,26 +38,28 @@ namespace BeersLib
 
         public List<Beer> BeersList { get; set; }
 
-        public Beers() {
+        public Beers () {
             this.BeersList = new List<Beer>();
-            this.BeersList.Add(new Beer("Fosters", "CUB", 4.5f, 12, 350, 6f));
-            this.BeersList.Add(new Beer("VB", "CUB", 4.5f, 16, 375, 5f));
-            this.BeersList.Add(new Beer("Colonial Pale", "Colonial Brewing", 4.4f, 35, 375, 7f));
+            this.BeersList.Add(new Beer ("Fosters", "CUB", 4.5f, 12, 350, 6f));
+            this.BeersList.Add(new Beer ("VB", "CUB", 4.5f, 16, 375, 5f));
+            this.BeersList.Add(new Beer ("Colonial Pale", "Colonial Brewing", 4.4f, 35, 375, 7f));
             this.BeersList.Add(new Beer("Coopers", "Coopers", 3.5f, 5, 375, 5.5f));
         }
-        public Beers(List<Beer> beersList) {
+
+        public Beers (List<Beer> beersList) {
             BeersList = beersList;
         }
 
-        public Beer GetLightestBeer() {
+        public List<Beer> GetLightestBeers() {
             throw new NotImplementedException();
         }
 
-        public Beer CompareAbv() {
+        public List<Beer> GetHeaviestBeers() {
             throw new NotImplementedException();
         }
-        //return heaviest beer
-        public Beer GetHighestBeer() {
+
+        // return the heavier beer.  If equal, return beer1
+        public Beer CompareAbv(Beer beer1, Beer beer2) {
             throw new NotImplementedException();
         }
 
@@ -76,18 +71,18 @@ namespace BeersLib
             throw new NotImplementedException();
         }
 
-        public void UpdateBeer(Beer UpdateBeer) {
+        public void UpdateBeer(Beer updatedBeer) {
             throw new NotImplementedException();
         }
-        
+
         public Beer GetBeerByName(string name) {
             foreach(Beer b in this.BeersList) {
                 if(b.Name == name) {
                     return b;
                 }
             }
-
             return null;
         }
+
     }
 }
